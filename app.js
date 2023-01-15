baf7d0f86270baa17d9481bcd0a3c108820f6ec4
hyparh <hyparh@gmail.com>
2023-01-15 21:58:06 +0200

    API implemented

--- app.js
+++ app.js
@@ -25,7 +25,25 @@ app.get("/", (req, res) => {
 
 app.post("/convert-mp3", async (req, res) => {
     const videoId = req.body.videoID;
-    console.log(videoId);
+    if (videoId === undefined || videoId === "" || videoId === null) {
+        return res.render("index", { success: false, message: "Please enter a video ID" });
+    } else {
+        const fetchAPI = await fetch(`https://youtube-mp36.p.rapidapi.com/dl?id=${videoId}`, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": process.env.API_KEY,
+                "x-rapidapi-host": process.env.API_HOST
+            }
+        });
+
+        const fetchResponse = await fetchAPI.json();
+
+        if (fetchResponse.status === "ok") {
+            return res.render("index", { success: true, song_title: fetchResponse.title, song_link: fetchResponse.link });
+        } else {
+            return res.render("index", {success: false, message: fetchResponse.msg});
+        }
+    }
 });
 
 //start the server
